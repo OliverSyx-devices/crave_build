94702aa295afa29dbb1d18d8061a447d11dc0125
Rimuru <paleloebarbershop@gmail.com>
2025-09-01 21:43:10 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ rm -rf device/xiaomi/mojito
 rm -rf vendor/xiaomi/mojito
 rm -rf kernel/xiaomi/mojito
 rm -rf vendor/private/keys
+rm -rf vendor/xiaomi/mojito-leicacamera
 echo "=================="
 echo "Remove LOCAL MANIFEST success"
 echo "=================="
@@ -24,6 +25,12 @@ echo "=================="
 echo "=================="
 echo "Repo SYNC success"
 echo "=================="
+
+# Leica
+git clone --depth=1 https://gitlab.com/pnplusplus/android_vendor_xiaomi_mojito-leicacamera vendor/xiaomi/mojito-leicacamera
+echo "============================"
+echo "Clone LEICA success"
+echo "============================"
 
 # Set up build environment
 . build/envsetup.sh
